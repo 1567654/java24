34dd4e2a9aa462f53d37b3f85845291d3311afd7
maxsve18 <svensson.max@gmail.com>
2024-10-24 10:42:54 +0200

    added another println

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from William and Max!");
+
 	for (int i = 0; i < 5; i++) {
 		System.out.println("i");
 	}
+        System.out.println("Hope you have a good  day.");
     }
 }
